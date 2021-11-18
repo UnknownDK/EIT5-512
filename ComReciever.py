9a656bb40190010b2744c2d9ae098df764d79c4c
Greven <bhgreven@pm.me>
2021-11-18 11:56:19 +0100

    Some changes

--- ComReciever.py
+++ ComReciever.py
@@ -4,20 +4,21 @@ from datetime import datetime
 import csv
 
 ser = serial.Serial('COM6', 115200, timeout=1)
-startTime = time.time_ns()
-times = []
+times = [] #Har byttet rundt på navnene på dem her lol
 data = []
 
-
+input("Press Enter to continue...")
+startTime = time.time_ns()
 while True:
   try:
     memes = ser.readline().strip().decode('utf-8')
     currTime = time.time_ns() - startTime
     times.append(memes)
-    data.append(currTime/100000)
-    print(str(currTime/100000) + " " + str(memes))
+    data.append(currTime)
+    print(str(currTime) + " " + str(memes))
   except KeyboardInterrupt:
     break
+
 
 filename = str(datetime.now().strftime("%d%m%Y-%H%M%S")) + ".csv"
 
