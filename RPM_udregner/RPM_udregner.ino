d42db4589a370f27052bd427458397b5ce961910
Morten <mrnabe19@student.aau.dk>
2021-11-23 09:44:48 +0100

    færdig

--- RPM_udregner/RPM_udregner.ino
+++ RPM_udregner/RPM_udregner.ino
@@ -10,45 +10,47 @@ uint64_t currentMillis = 0;
 int interval = 0;
 float rpm_to_radians = 0.10471975512;
 float rad_to_deg = 57.29578;
-float ang_velocity = 0;   
+float ang_velocity = 0;
 float ang_velocity_deg = 0;
 
 
 void setup() {
-Serial.begin(19200); // activates the serial communication
-pinMode(Encoder_output_A,INPUT); // sets the Encoder_output_A pin as the input
-pinMode(Encoder_output_B,INPUT); // sets the Encoder_output_B pin as the input
-pinMode(Encoder_output_I,INPUT); // sets the Encoder_output_I pin as the input
-attachInterrupt(digitalPinToInterrupt(Encoder_output_A),DC_Motor_Encoder,RISING);
-attachInterrupt(digitalPinToInterrupt(Encoder_output_I),DC_Motor_Reset,RISING);
+  Serial.begin(19200); // activates the serial communication
+  pinMode(Encoder_output_A, INPUT); // sets the Encoder_output_A pin as the input
+  pinMode(Encoder_output_B, INPUT); // sets the Encoder_output_B pin as the input
+  pinMode(Encoder_output_I, INPUT); // sets the Encoder_output_I pin as the input
+  attachInterrupt(digitalPinToInterrupt(Encoder_output_A), DC_Motor_Encoder, RISING);
+  attachInterrupt(digitalPinToInterrupt(Encoder_output_I), DC_Motor_Reset, RISING);
 }
 
 void loop() {
   //Serial.println(Count_pulses);
-  currentMillis = millis();
-  Serial.println(RPM);
+
+  Serial.println(interval);
+  Serial.println(RPM, 7);
   if (currentMillis - previousMillis > interval) {
-   //RPM_Motor();
+    //RPM_Motor();
   }
 }
 
-void DC_Motor_Encoder(){
+void DC_Motor_Encoder() {
   int b = digitalRead(Encoder_output_B);
-  if(b > 0){
+  if (b > 0) {
     Count_pulses++;
   }
-  else{
+  else {
     Count_pulses--;
   }
   RPM_Motor();
 }
 
-void DC_Motor_Reset(){ //pulser pr omgang
+void DC_Motor_Reset() { //pulser pr omgang
   //Serial.println(Count_pulses);
   //Count_pulses = 0;
 }
-void RPM_Motor(){
+void RPM_Motor() {
+  currentMillis = micros();
   interval = currentMillis - previousMillis;
-  RPM= (1.0/(interval/1000))/500;  
+  RPM = (60000000.0 / interval * 500.0);
   previousMillis = currentMillis;
 }
