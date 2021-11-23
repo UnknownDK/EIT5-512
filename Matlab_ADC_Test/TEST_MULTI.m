038d990d8a05b367ea049ac79997de2558c4098a
Nikolaj <nikolajkgpth@gmail.com>
2021-11-23 23:44:18 +0100

    Fikset test script

--- Matlab_ADC_Test/TEST_MULTI.m
+++ Matlab_ADC_Test/TEST_MULTI.m
@@ -3,9 +3,9 @@ clearvars
 tic
 
 %signaler
-f1 = 300e6;
+f1 = 286.4e6;
 theta_a = pi; 
-f2 = 300e6;
+f2 = 286.4e6;
 theta_b = pi/2;
 
 Clambda = 0.1228658;
@@ -13,7 +13,7 @@ vOensket = (Clambda * 90)/(360 * 0.75 *Clambda);
 droneVinkelOensket = (asin(vOensket)) * 180 / pi
 
 
-fs = 42e6;
+fs = 41e6;
 sample_range = [256 256 2048]; %antal samples vi tester?? 
 snr_range = [46 1 49]; %hvad tester vi af SNR f
 b_range = [12 2 12];      %hvad bits tester vi med
@@ -24,7 +24,7 @@ antalSNR = round((snr_range(3) - snr_range(1)) / snr_range(2));
 
 
 filterDC = 10;
-samples = 42e3;
+samples = 2^14;
 Tmax = samples/fs;
 t = 0:1/fs:Tmax-1/fs;
 
@@ -48,8 +48,8 @@ Bits = 8;          % Bit number.
 for run_E = 1:1:length(e_range)
     eInput = e_range(1) - 0.05 + (run_E * 0.05);       % % af input range vi bruger
     %Signaler
-    a = (cos((2*pi*f1*t)+theta_a)+cos((2*pi*301e6*t)+theta_a));
-    b = cos((2*pi*f2*t)+theta_b);
+    a = (cos((2*pi*f1*t)+theta_a)+cos((2*pi*301e6*t)+theta_a))+cos((2*pi*289e6*t)+theta_a)+cos((2*pi*299e6*t)+theta_a)+cos((2*pi*290.6e6*t)+theta_a);
+    b = cos((2*pi*f2*t)+theta_b)+cos((2*pi*301e6*t)+theta_b)+cos((2*pi*289e6*t)+theta_b)+cos((2*pi*299e6*t)+theta_b)+cos((2*pi*290.6e6*t)+theta_b);
 
     aSampled = eInput*rescale(a,-1,1);
     bSampled = eInput*rescale(b,-1,1); 
@@ -79,18 +79,24 @@ for run_E = 1:1:length(e_range)
     %thetaB = angle(B);
     
     [maxAmpValB, indexB] = max(abs(B));
-    %[maxAmpValA, indexA]= max(abs(A));
     faseB(1) = angle(B(indexB)) * 180 / pi;
     faseA(1) = angle(A(indexB)) * 180 / pi;
     B(indexB) = 0;
-    [maxAmpValB, indexA] = max(abs(B));
+    A(indexB) = 0;
+    [maxAmpValA, indexA] = max(abs(B));
     faseB(2) = angle(B(indexA)) * 180 / pi;
     faseA(2) = angle(A(indexA)) * 180 / pi;
+    B(indexA) = 0;
+    A(indexA) = 0;
+    [maxAmpValC, indexC] = max(abs(B));
+    faseB(3) = angle(B(indexC)) * 180 / pi;
+    faseA(3) = angle(A(indexC)) * 180 / pi;
     
     fase(1) = mod(abs(faseB(1) - faseA(1)),180);
     fase(2) = mod(abs(faseB(2) - faseA(2)),180);
+    fase(3) = mod(abs(faseB(3) - faseA(3)),180);
 
-    faseDif(run_E) = mean(fase,"all");
+    faseDif(run_E) = mean(fase,'all');
     v(run_E) = (Clambda * faseDif(run_E))/(360 * 0.75 *Clambda);
     droneVinkel(run_E) = (asin(v(run_E))) * 180 / pi
     difference(run_E) = droneVinkelOensket - droneVinkel(run_E);
