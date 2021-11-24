7f7ab78d7c0df162dc9206a304154a0bf395fa73
Nikolaj <nikolajkgpth@gmail.com>
2021-11-24 23:52:13 +0100

    Tilfoejet stoej til sim
    Man kan vaelge antal test mm. bad description. Bjørn hjælp med branches

--- Matlab_ADC_Test/TEST_MULTI.m
+++ Matlab_ADC_Test/TEST_MULTI.m
@@ -13,17 +13,18 @@ vOensket = (Clambda * 90)/(360 * 0.75 *Clambda);
 droneVinkelOensket = (asin(vOensket)) * 180 / pi;
 
 
-fs = 41.1e6;
+fs = 42.9e6;
 sample_range = [256 256 2048]; %antal samples vi tester?? 
-snr_range = [46 1 49]; %hvad tester vi af SNR f
+snr_range = [10 20]; %hvad tester vi af SNR f
 b_range = [12 2 12];      %hvad bits tester vi med
+e_range = [0.9:0.05:0.9];
 
 antalSample = round((sample_range(3) - sample_range(1)) / sample_range(2));
 antalB = round((b_range(3) - b_range(1)) / b_range(2));
-antalSNR = round((snr_range(3) - snr_range(1)) / snr_range(2));
+antalSNR = length(snr_range);
+antalTests = 1;
 
-
-filterDC = 32;
+filterDC = 128;
 samples = 2^17;
 Tmax = samples/fs;
 t = 0:1/fs:Tmax-1/fs;
@@ -34,67 +35,83 @@ N = samples;           % oploesning af FFT
 f = (-N/2:N/2-1)/N*fs;
 
 
-difResult = zeros(antalB,antalSNR);
+difResult = zeros(1,antalSNR);
 faseResult = zeros(antalB,antalSNR);
 droneResult = zeros(antalB,antalSNR);
 
+
 %for i_b = b_range(1):b_range(2):b_range(3)
-%for i_e = snr_range(1):snr_range(2):snr_range(3)
-e_range = [0.9:0.05:0.9];
+%
+
     
 Bits = 12;          % Bit number.    
+eInput = 0.9;
 
-    
-for run_E = 1:1:length(e_range)
-    eInput = e_range(1) - 0.05 + (run_E * 0.05);       % % af input range vi bruger
-    %Signaler
-    a = signalGen(f1,theta_a,t);
-    b = signalGen(f1,theta_b,t);%cos((2*pi*300e6*t)+theta_b)+cos((2*pi*301e6*t)+theta_b)+cos((2*pi*289e6*t)+theta_b)+cos((2*pi*299e6*t)+theta_b)+cos((2*pi*293.6e6*t)+theta_b);
+%Signaler
+a = signalGen(f1,theta_a,t);
+b = signalGen(f1,theta_b,t);%cos((2*pi*300e6*t)+theta_b)+cos((2*pi*301e6*t)+theta_b)+cos((2*pi*289e6*t)+theta_b)+cos((2*pi*299e6*t)+theta_b)+cos((2*pi*293.6e6*t)+theta_b);
 
-    aSampled = eInput*rescale(a,-1,1);
-    bSampled = eInput*rescale(b,-1,1); 
+aSampled = eInput*rescale(a,-1,1);
+bSampled = eInput*rescale(b,-1,1); 
+
+for run_SNR = 1:1:antalSNR
+    snrLevel = snr_range(1) - 10 + (run_SNR * 10);
     
-    % ADC: Quantizer
-    % Quantizing (abs of input value not over 1)
-    partition = [-1:(2)/(2^Bits):1-(2)/(2^Bits)];
-    codebook = [-1:(2)/(2^Bits):1-(2)/(2^Bits)];
-    aDigital = quantiz(aSampled,partition,codebook); 
-    bDigital = quantiz(bSampled,partition,codebook);    
+    faseDif = zeros(antalTests,1);
+    v = zeros(antalTests,1);
+    droneVinkel = zeros(antalTests,1);
+    difference = zeros(antalTests,1);
+    parfor runI = 1:antalTests
     
+        aNoise = awgn(aSampled,snrLevel,'measured');
+        bNoise = awgn(bSampled,snrLevel,'measured');
     
-    %fft stuff
-    A = fft(aDigital,N);
-    A = fftshift(A);
-    B = fft(bDigital,N);
-    B = fftshift(B);
+        % ADC: Quantizer
+        % Quantizing (abs of input value not over 1)
+        partition = [-1:(2)/(2^Bits):1-(2)/(2^Bits)];
+        codebook = [-1:(2)/(2^Bits):1-(2)/(2^Bits)];
+        aDigital = quantiz(aNoise,partition,codebook); 
+        bDigital = quantiz(bNoise,partition,codebook);    
+
+
+        %fft stuff
+        A = fft(aDigital,N);
+        A = fftshift(A);
+        B = fft(bDigital,N);
+        B = fftshift(B);
+
+        %sorterer DC tillaegget fra ADC'erne fra:
+        for i = 1:1:(N/2)+(N/filterDC)   %(N/2)-(N/filterDC)
+            A(i) = 0;
+            B(i) = 0;
+        end
+
+        difAntal = 12;
+        fase = zeros(1,difAntal);
+        index = zeros(1,difAntal);
+        faseA = zeros(1,difAntal);
+        faseB = zeros(1,difAntal);
+        for i = 1:1:difAntal
+            [maxAmpValB, index(i)] = max(abs(B));
+            faseB(i) = angle(B(index(i))) * 180 / pi;
+            faseA(i) = angle(A(index(i))) * 180 / pi;
+            B(index(i)) = 0;
+            A(index(i)) = 0;
+            fase(i) = mod(abs(faseB(i) - faseA(i)),180);
+        end
+
+
+        faseDif(runI) = mean(fase,"all");
+        v(runI) = (Clambda * faseDif(runI))/(360 * 0.75 *Clambda);
+        droneVinkel(runI) = (asin(v(runI))) * 180 / pi;
+        difference(runI) = droneVinkelOensket - droneVinkel(runI);
     
-    %sorterer DC tillaegget fra ADC'erne fra:
-    for i = 1:1:(N/2)+(N/filterDC)   %(N/2)-(N/filterDC)
-        A(i) = 0;
-        B(i) = 0;
+
     end
-    
-    difAntal = 12;
-    fase = zeros(1,difAntal);
-    index = zeros(1,difAntal);
-    faseA = zeros(1,difAntal);
-    faseB = zeros(1,difAntal);
-    for i = 1:1:difAntal
-        [maxAmpValB, index(i)] = max(abs(B));
-        faseB(i) = angle(B(index(i))) * 180 / pi;
-        faseA(i) = angle(A(index(i))) * 180 / pi;
-        B(index(i)) = 0;
-        A(index(i)) = 0;
-        fase(i) = mod(abs(faseB(i) - faseA(i)),180);
-    end
-     
-        
-
-
-    faseDif(run_E) = mean(fase,"all");
-    v(run_E) = (Clambda * faseDif(run_E))/(360 * 0.75 *Clambda);
-    droneVinkel(run_E) = (asin(v(run_E))) * 180 / pi
-    difference(run_E) = droneVinkelOensket - droneVinkel(run_E);
+    difference = abs(difference);
+    difResult(run_SNR) = mean(difference,'all');
+    %faseResult(b_taeller,snr_taeller) = mean(faseDif,'all');
+    %droneResult(b_taeller,snr_taeller) = mean(droneVinkel,'all');
     
 end
 % faseDif(runI) = mod(abs(faseB - faseA),180);
